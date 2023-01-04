398d05f352653ee994059034ce30e30ce887bb6d
Kitsune <121882856+Ki2sune01@users.noreply.github.com>
2023-01-04 16:01:18 +0300

    with menu

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
 from kivy.lang import Builder
 
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.app import MDApp
 
 KV = '''
-<DrawerClickableItem@MDNavigationDrawerItem>
-    focus_color: "#FFB6C1"
-    text_color: "#4a4939"
-    icon_color: "#4a4939"
-    ripple_color: "#c5bdd2"
-    selected_color: "#0000FF"
-
-
-<DrawerLabelItem@MDNavigationDrawerItem>
-    text_color: "#4a4939"
-    icon_color: "#4a4939"
-    focus_behavior: False
-    selected_color: "#4a4939"
-    _no_ripple_effect: True
-
-
 MDScreen:
 
     MDNavigationLayout:
@@ -28,58 +13,31 @@ MDScreen:
             MDScreen:
 
                 MDTopAppBar:
-                    title: "Выбор таблицы"
+                    title: "Contents"
                     elevation: 4
                     pos_hint: {"top": 1}
-                    md_bg_color: "#e7e4c0"
-                    specific_text_color: "#4a4939"
-                    left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
+                    md_bg_color: "#C486E9"
+                    specific_text_color: "#000000"
+                    left_action_items:
+                        [['menu', lambda x: nav_drawer.set_state("open")]]
+
 
         MDNavigationDrawer:
             id: nav_drawer
             radius: (0, 16, 16, 0)
 
-            MDNavigationDrawerMenu:
-
-                MDNavigationDrawerHeader:
-                    title: "Таблицы"
-                    title_color: "#4a4939"
-                    text: "Ещё таблицы"
-                    spacing: "4dp"
-                    padding: "12dp", 0, 0, "56dp"
-
-                MDNavigationDrawerLabel:
-                    text: "Таблицы"
-
-                DrawerClickableItem:
-                    icon: "gmail"
-                    right_text: ""
-                    text_right_color: "#4a4939"
-                    text: "Таблица 1"
-
-                DrawerClickableItem:
-                    icon: "send"
-                    text: "Таблица 2"
-
-                MDNavigationDrawerDivider:
-
-                MDNavigationDrawerLabel:
-                    text: "Labels"
-
-                DrawerLabelItem:
-                    icon: "information-outline"
-                    text: "Label"
-
-                DrawerLabelItem:
-                    icon: "information-outline"
-                    text: "Label"
+            ContentNavigationDrawer:
 '''
 
 
-class ChT(MDApp):
+class ContentNavigationDrawer(MDBoxLayout):
+    pass
+
+
+class ChTApp(MDApp):
     def build(self):
-        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.theme_style = "Light"
         return Builder.load_string(KV)
 
 
-ChT().run()
+ChTApp().run()
